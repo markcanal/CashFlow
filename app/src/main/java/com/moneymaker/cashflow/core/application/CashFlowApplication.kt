2c7e1ea0e01f7a31bc9d90293bf7a56955cb9abb
Mark <markjeraldc@gmail.com>
2024-03-09 12:15:05 +0800

    Revert ":tada: Project CashFlow created"
    This reverts commit c1c10c781b22924acd6ead21cfc9176707c1cbfb.

--- app/src/main/java/com/moneymaker/cashflow/core/application/CashFlowApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.moneymaker.cashflow.core.application
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class CashFlowApplication : Application()
