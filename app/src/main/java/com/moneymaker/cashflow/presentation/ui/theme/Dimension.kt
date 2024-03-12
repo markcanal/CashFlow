dbf281a484a1d0406f3d9bc27f9ad858cf1a56c2
Mark <markjeraldc@gmail.com>
2024-03-12 23:12:40 +0800

    :sparkles: [CF-#3] Add Dimension

---
+++ app/src/main/java/com/moneymaker/cashflow/presentation/ui/theme/Dimension.kt
@@ -0,0 +1,16 @@
+package com.moneymaker.cashflow.presentation.ui.theme
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+data class Dimension(
+    val default: Dp = 0.dp,
+    val pointFiveSp: TextUnit = 0.5.sp,
+    val twelveSp: TextUnit = 12.sp,
+    val fourteenSp: TextUnit = 14.sp,
+    val sixteenSp: TextUnit = 16.sp,
+    val eighteenSp: TextUnit = 18.sp,
+    val twentyFourSp: TextUnit = 24.sp
+)
