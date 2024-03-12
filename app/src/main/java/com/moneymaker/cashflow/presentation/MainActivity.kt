ea6046a50d8fd4e615e2179b62c336b6d784e799
Mark <markjeraldc@gmail.com>
2024-03-12 23:16:21 +0800

    :recycle: [CF-#3] Refactor main activiity for initial screen routing

--- app/src/main/java/com/moneymaker/cashflow/presentation/MainActivity.kt
+++ app/src/main/java/com/moneymaker/cashflow/presentation/MainActivity.kt
@@ -3,43 +3,49 @@ package com.moneymaker.cashflow.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.moneymaker.cashflow.presentation.ui.theme.CashFlowTheme
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.moneymaker.cashflow.core.application.Destination
+import com.moneymaker.cashflow.presentation.screen.home.HomeScreen
+import com.moneymaker.cashflow.presentation.screen.splashscreen.SplashScreen
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlin.time.Duration.Companion.seconds
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            CashFlowTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+            val navController = rememberNavController()
+            var splashTime by remember { mutableIntStateOf(0) }
+            LaunchedEffect(key1 = splashTime) {
+                while (splashTime <= 5) {
+                    delay(1.seconds)
+                    splashTime++
+                }
+            }
+            NavHost(
+                navController = navController, startDestination = if (splashTime <= 5) {
+                    Destination.Splash.route
+                } else {
+                    Destination.Home.route
+                }
+            ) {
+                composable(Destination.Splash.route) {
+                    SplashScreen()
+                }
+                composable(Destination.Home.route) {
+                    HomeScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CashFlowTheme {
-        Greeting("Android")
     }
 }
