6e4c744e03943d2ddcf2d62e9f1b28c90472c229
Mark <markjeraldc@gmail.com>
2024-03-12 12:44:42 +0800

    :recycle: [CF-#2] Implement Timber on application

--- app/src/main/java/com/moneymaker/cashflow/core/application/CashFlowApplication.kt
+++ app/src/main/java/com/moneymaker/cashflow/core/application/CashFlowApplication.kt
@@ -1,7 +1,16 @@
 package com.moneymaker.cashflow.core.application
 
 import android.app.Application
+import com.moneymaker.cashflow.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class CashFlowApplication : Application()
+class CashFlowApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
