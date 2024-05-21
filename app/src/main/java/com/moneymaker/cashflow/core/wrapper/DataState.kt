e104bc10199e619655db63257a933a33d0a8879b
Mark <markjeraldc@gmail.com>
2024-05-22 03:56:56 +0800

    :recycle: [CF #3] Change to out T

--- app/src/main/java/com/moneymaker/cashflow/core/wrapper/DataState.kt
+++ app/src/main/java/com/moneymaker/cashflow/core/wrapper/DataState.kt
@@ -1,7 +1,7 @@
 package com.moneymaker.cashflow.core.wrapper
 
-sealed class DataState<T : Any> {
-    class LOADING<T : Any> : DataState<T>()
+sealed class DataState<out T> {
+    data object LOADING : DataState<Nothing>()
     class SUCCESS<T : Any>(val data: T) : DataState<T>()
     class ERROR<T : Any>(val title: String?, val message: String?) : DataState<T>()
 }
